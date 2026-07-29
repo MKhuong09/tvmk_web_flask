8abd0e74386f7aa4353aced4d2b541b8be78eef1
MKhuong09 <manhkhuong1234@gmail.com>
2026-07-29 21:41:20 +0700

    fix login issue

--- website/views.py
+++ website/views.py
@@ -1,16 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
-#@login_required
+@login_required
 def home():
     return render_template('home.html', user=current_user)
-
-
-from flask import Blueprint, render_template
-from .models import User, Registration  # Import các Model cần dùng
-from . import db
-
-# Giả sử blueprint của em tên là views
